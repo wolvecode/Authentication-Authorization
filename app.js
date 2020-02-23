ec3bdc966b9acd6d2be4e9ff01b731d738936f85
wolvecode <wolvecode@gmail.com>
2020-02-23 22:55:41 +0100

    auth routes add

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 const express = require('express')
-const next = require('next')
-
-const dev = process.env.NODE_ENV !== 'production'
-const nextApp = next({ dev })
-const handle = nextApp.getRequestHandler()
-
+const app = express()
 const port = 8000
 
-const app = express()
-
-app.listen(port, () => {
-  console.log(`server lsitening at port ${port}`)
+app.get('/', (req, res) => {
+  res.send('Hello world')
 })
 
-nextApp.prepare().then(() => {
-  const app = express()
-
-  app.get('/my-custom-route', (req, res) =>
-    res.status(200).json({ hello: 'Hello, from the back-end world!' })
-  )
-
-  app.get('/', (req, res) => {
-    res.send('Hello world')
-  })
+app.listen(port, () => {
+  console.log(`Server staring at port : ${port}`)
 })
---
+++ server/routes/auth.routes.js
@@ -0,0 +1,17 @@
+const express = require('Eexpress')
+
+const router = express.Router()
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body
+
+  return res.status(200).json({ success: true, data })
+})
+
+router.post('/register', async (req, res) => {
+  const { firstName, lastName, email, password } = req.body
+
+  return res.status(200).json({ success: true, data: null })
+})
+
+export default router
