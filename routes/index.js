a89bb60f6d247156576ce532ba11f24b010ca1bf
wolvecode <wolvecode@gmail.com>
2020-03-12 23:22:11 +0100

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,50 +1,62 @@
+const userController = require('../controller/userController')
 const LocalStrategy = require('passport-local').Strategy
+const express = require('express')
+const router = express.Router()
 
-module.exports = (app, passport) => {
-  app.get('/dashboard', isLoggedIn, (req, res) => {
-    res.render('dashboard')
-  })
+router.get('/login', userController.loginForm)
+router.get('/register', userController.registerForm)
+//validate
+//regsiter the user
+//login on register
+router.post('/register', userController.validateRegister)
 
-  app.get('/login', (req, res) => {
-    res.render('login')
-  })
+module.exports = router
 
-  app.get('/logout', (req, res) => {
-    req.logout()
-    res.redirect('/login')
-  })
+// module.exports = (app, passport) => {
+//   app.get('/dashboard', isLoggedIn, (req, res) => {
+//     res.render('dashboard')
+//   })
 
-  app.post(
-    '/login',
-    passport.authenticate('local', {
-      failureRedirect: '/login',
-      successRedirect: '/dashboard'
-    })
-  )
+//   app.get('/login', (req, res) => {
+//     res.render('login')
+//   })
 
-  passport.use(
-    new LocalStrategy((username, password, done) => {
-      if (username === 'biodun@gmail.com' && password === '1234') {
-        return done(null, { username: 'biodun@gmail.com' })
-      } else {
-        return done(null, false)
-      }
-    })
-  )
+//   app.get('/logout', (req, res) => {
+//     req.logout()
+//     res.redirect('/login')
+//   })
 
-  passport.serializeUser((user, done) => {
-    done(null, user.username)
-  })
+//   app.post(
+//     '/login',
+//     passport.authenticate('local', {
+//       failureRedirect: '/login',
+//       successRedirect: '/dashboard'
+//     })
+//   )
 
-  passport.deserializeUser((username, done) => {
-    done(null, { username: username })
-  })
+//   passport.use(
+//     new LocalStrategy((username, password, done) => {
+//       if (username === 'biodun@gmail.com' && password === '1234') {
+//         return done(null, { username: 'biodun@gmail.com' })
+//       } else {
+//         return done(null, false)
+//       }
+//     })
+//   )
 
-  function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next()
-    } else {
-      return res.redirect('/login')
-    }
-  }
-}
+//   passport.serializeUser((user, done) => {
+//     done(null, user.username)
+//   })
+
+//   passport.deserializeUser((username, done) => {
+//     done(null, { username: username })
+//   })
+
+//   function isLoggedIn(req, res, next) {
+//     if (req.isAuthenticated()) {
+//       return next()
+//     } else {
+//       return res.redirect('/login')
+//     }
+//   }
+// }
