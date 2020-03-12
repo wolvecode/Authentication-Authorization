02153f3f1a76dfb77655e046b307315a21d13371
wolvecode <wolvecode@gmail.com>
2020-03-12 23:20:21 +0100

    User controller

---
+++ controller/userController.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+exports.loginForm = (req, res) => {
+  res.render('login', { title: 'login' })
+}
+
+exports.registerForm = (req, res) => {
+  res.render('register', { title: 'register' })
+}
+
+exports.validateRegister = function(req, res, next) {
+  req.sanitizeBody('name')
+  req.checkBody('name', 'name is required').notEmpty()
+  req.checkBody('email', 'Email supply is not valid').isEmail()
+  req.sanitizeBody('email').normalizeEmail({
+    remove_dots: false,
+    remove_extenstion: false,
+    gmail_remove_subaddress: false
+  })
+  req.checkBody('password', 'Password can not blank').notEmpty()
+  req
+    .checkBody('password-confirm', 'Oops! Your password do not match')
+    .equals(req.body.password)
+
+  const errors = req.validationErrors()
+  if (errors) {
+    req.flash(
+      'error',
+      errors.map(err => err.msg)
+    )
+    res.render('register', {
+      title: 'register',
+      body: req.body,
+      flashes: req.flash()
+    })
+    return
+  }
+  next()
+}
