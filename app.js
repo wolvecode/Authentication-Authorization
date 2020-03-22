7816ee503282808ac8900fe7830130f0d6b644c2
wolvecode <wolvecode@gmail.com>
2020-03-22 23:14:42 +0100

    connect all module and middleware

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const app = express()
+const UserModel = require('./model/User')
 const routes = require('./routes/routes')
 const secureRoute = require('./routes/secureRoute')
-const app = express()
-const userModel = require('./model/User')
 
-mongoose.connect('mongodb://127.0.0.1:27017/passport-jwt', {
-  useMongoClient: true
-})
-mongoose.connection.on('error', error => console.log(error))
-mongoose.Promise = global.Promise
-
-require('./auth/auth')
+require('./connect')
+require('./middleware/authenticate')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -25,8 +19,4 @@ app.use('/user', passport.authenticate('jwt', { session: false }), secureRoute)
 app.use(function(err, req, res, next) {
   res.status(err.status || 500)
   res.json({ error: err })
-})
-
-app.listen(3000, () => {
-  console.log('Server started')
 })
