ef881e2dd7de2d277e2379607a907778c004cdaf
wolvecode <wolvecode@gmail.com>
2020-03-26 13:32:25 +0100

    server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const app = require('./app')
+const express = require('express')
+const app = express()
+require('./app')
 
-//Starting server at port 5000
+//Starting server at port 4000
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
